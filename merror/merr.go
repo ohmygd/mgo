1bcb75d37ff7accccf493264ea41ae3cb39b3c95
马超 <178069409@qq.com>
2019-08-28 11:28:39 +0800

    err

---
+++ merror/merr.go
@@ -0,0 +1,24 @@
+package merror
+
+type Merr struct {
+	code int
+	msg string
+}
+
+func New(code int) *Merr {
+	return &Merr{
+		code:code,
+	}
+}
+
+func (m *Merr)GetCode() int {
+	return m.code
+}
+
+func (m *Merr)GetMsg() string {
+	return m.msg
+}
+
+func (m *Merr)Error() string{
+	return m.msg
+}
