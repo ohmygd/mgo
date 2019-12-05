86d503be441bbd655f7153e55fffe9ea5e2912fe
马超 <178069409@qq.com>
2019-12-05 15:01:14 +0800

    mongo 认证修改

--- dao/mongo/daoMongo.go
+++ dao/mongo/daoMongo.go
@@ -9,15 +9,22 @@ import (
 	"github.com/ohmygd/mgo/config"
 	"gopkg.in/mgo.v2"
 	"strings"
+	"sync"
 	"time"
 )
 
-var session *mgo.Session
+var sessionMap map[string]*mgo.Session
+var once sync.Once
 
 type DaoMongo struct {
+	Db string
 }
 
-func init() {
+func (d *DaoMongo) getSession() (*mgo.Session, error) {
+	if sessionMap != nil && sessionMap[d.Db] != nil {
+		return sessionMap[d.Db], nil
+	}
+
 	hostInfo := config.GetMongoMsg("hosts").(string)
 	username := config.GetMongoMsg("username").(string)
 	password := config.GetMongoMsg("password").(string)
@@ -34,22 +41,56 @@ func init() {
 		PoolLimit: poolLimit,
 	}
 
-	if username != "" && password != "" {
-		dialInfo.Username = username
-		dialInfo.Password = password
-	}
-
+	var session *mgo.Session
 	var err error
 
 	session, err = mgo.DialWithInfo(dialInfo)
-
 	if err != nil {
 		panic(err.Error())
 	}
 
+	if username != "" && password != "" {
+		err = session.DB(d.Db).Login(username, password)
+		if err != nil {
+			panic(err)
+		}
+	}
+
 	session.SetMode(mgo.Monotonic, true)
+
+	once.Do(func() {
+		sessionMap = make(map[string]*mgo.Session, 0)
+	})
+
+	sessionMap[d.Db] = session
+
+	return session, nil
 }
 
+func (d *DaoMongo) GetDb() *mgo.Database {
+	session, err := d.getSession()
+	if err != nil {
+		panic(err)
+	}
+
+	return session.DB(d.Db)
+}
+
+// todo Session close 暂时没发现哪里需要用到
+
 func (d *DaoMongo) NewSession() *mgo.Session {
+	session, err := d.getSession()
+	if err != nil {
+		panic(err)
+	}
+
+	return session
+}
+
+func (d *DaoMongo) SessionCopy() *mgo.Session {
+	session, err := d.getSession()
+	if err != nil {
+		panic(err)
+	}
 	return session.Copy()
 }
